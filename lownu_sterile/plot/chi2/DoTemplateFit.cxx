0118346116a5af69cab6db1f9582f8d142221bc4
qvuong <qvuong@ur.rochester.edu>
2024-06-30 10:18:52 -0500

    chi2 surfaces for 4 parameters

---
+++ lownu_sterile/plot/chi2/DoTemplateFit.cxx
@@ -0,0 +1,161 @@
+#include "TemplateFitter.cxx"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH2.h"
+#include "TF1.h"
+#include "TStyle.h"
+#include <TRandom.h>
+#include <list>
+
+int main()
+{
+  const char data_path[] = "/exp/dune/app/users/qvuong/data/lownu";
+
+  TFile *ftP_m = new TFile(Form("%s/LEdep/fitPara_m.root", data_path),"READ");
+  TFile *ftP_e = new TFile(Form("%s/LEdep/fitPara_e.root", data_path),"READ");
+
+  TTree *tree_m = (TTree*)ftP_m->Get("pardir");
+  TTree *tree_e = (TTree*)ftP_e->Get("pardir");
+
+  double para0_m,para1_m,para2_m,para3_m,para4_m,para5_m,norm_m,fitPara_m[29][7];
+  double para0_e,para1_e,para2_e,para3_e,para4_e,para5_e,norm_e,fitPara_e[29][7];
+
+  tree_m->SetBranchAddress("para0",&para0_m);
+  tree_m->SetBranchAddress("para1",&para1_m);
+  tree_m->SetBranchAddress("para2",&para2_m);
+  tree_m->SetBranchAddress("para3",&para3_m);
+  tree_m->SetBranchAddress("para4",&para4_m);
+  tree_m->SetBranchAddress("para5",&para5_m);
+  tree_m->SetBranchAddress("norm",&norm_m);
+
+  tree_e->SetBranchAddress("para0",&para0_e);
+  tree_e->SetBranchAddress("para1",&para1_e);
+  tree_e->SetBranchAddress("para2",&para2_e);
+  tree_e->SetBranchAddress("para3",&para3_e);
+  tree_e->SetBranchAddress("para4",&para4_e);
+  tree_e->SetBranchAddress("para5",&para5_e);
+  tree_e->SetBranchAddress("norm",&norm_e);
+
+  for(int i=0;i<tree_m->GetEntries();i++){
+    tree_m->GetEntry(i);
+    fitPara_m[i][0] = para0_m;
+    fitPara_m[i][1] = para1_m;
+    fitPara_m[i][2] = para2_m;
+    fitPara_m[i][3] = para3_m;
+    fitPara_m[i][4] = para4_m;
+    fitPara_m[i][5] = para5_m;
+    fitPara_m[i][6] = norm_m;
+  }
+  ftP_m->Close();
+
+  for(int i=0;i<tree_e->GetEntries();i++){
+    tree_e->GetEntry(i);
+    fitPara_e[i][0] = para0_e;
+    fitPara_e[i][1] = para1_e;
+    fitPara_e[i][2] = para2_e;
+    fitPara_e[i][3] = para3_e;
+    fitPara_e[i][4] = para4_e;
+    fitPara_e[i][5] = para5_e;
+    fitPara_e[i][6] = norm_e;
+  }
+  ftP_e->Close();
+
+  TFile *f = new TFile(Form("%s/LEdep/LE_1112_2.root", data_path),"READ");
+  TH2D *LvsE_e = (TH2D*)f->Get("h_e");
+  TH2D *LvsE_m = (TH2D*)f->Get("h_m");
+  TH1D *LEdep_e[29], *LEdep_m[29];
+  for(int i = 0; i < 29; i++) {
+    LEdep_e[i] = (TH1D*)LvsE_e->ProjectionY(Form("LE_e_bin%d",i+1),i+1,i+1);
+    LEdep_e[i]->Scale(1./LEdep_e[i]->Integral("width"));
+
+    LEdep_m[i] = (TH1D*)LvsE_m->ProjectionY(Form("LE_m_bin%d",i+1),i+1,i+1);
+    LEdep_m[i]->Scale(1./LEdep_m[i]->Integral("width"));
+  }
+
+
+  char var[20] = "ElepReco";
+  int nuCut = 3;
+
+  TFile *CC_f  = new TFile(Form("%s/input_dfiles/CC_output_56bins.root",data_path),"READ");
+  TFile *nue_f = new TFile(Form("%s/input_dfiles/nue_output_8bins.root",data_path),"READ");
+
+  TH2D* CC_hm    = (TH2D*)CC_f->Get(Form("m_h%sVsEv%d",var,nuCut));
+  TH2D* CC_hm_nc = (TH2D*)CC_f->Get(Form("nc_m_h%sVsEv%d",var,nuCut));
+  TH2D* CC_he    = (TH2D*)CC_f->Get(Form("e_h%sVsEv%d",var,nuCut));
+  TH2D* nue_hm   = (TH2D*)nue_f->Get(Form("m_h%sVsEv0",var));
+  TH2D* nue_hm_w = (TH2D*)nue_f->Get(Form("m_h%sVsEv0_w",var));
+  TH2D* nue_he   = (TH2D*)nue_f->Get(Form("e_h%sVsEv0",var));
+  TH2D* nue_he_w = (TH2D*)nue_f->Get(Form("e_h%sVsEv0_w",var));
+
+  TH1D * CC_templates_m[nbins_Ev];
+  TH1D * CC_templates_m_nc[nbins_Ev];
+  TH1D * CC_templates_e[nbins_Ev];
+  TH1D * nue_templates_m[nbins_Ev];
+  TH1D * nue_templates_m_w[nbins_Ev];
+  TH1D * nue_templates_e[nbins_Ev];
+  TH1D * nue_templates_e_w[nbins_Ev];
+
+  for(int i=0; i<nbins_Ev; i++) {
+    CC_templates_m[i]    = (TH1D*)CC_hm->ProjectionY(Form("CC_m_bin%d",i+1),i+1,i+1);
+    CC_templates_m_nc[i] = (TH1D*)CC_hm_nc->ProjectionY(Form("CC_nc_m_bin%d",i+1),i+1,i+1);
+    CC_templates_e[i]    = (TH1D*)CC_he->ProjectionY(Form("CC_e_bin%d",i+1),i+1,i+1);
+    nue_templates_m[i]    = (TH1D*)nue_hm->ProjectionY(Form("nue_m_bin%d",i+1),i+1,i+1);
+    nue_templates_m_w[i]  = (TH1D*)nue_hm_w->ProjectionY(Form("nue_w_m_bin%d",i+1),i+1,i+1);
+    nue_templates_e[i]    = (TH1D*)nue_he->ProjectionY(Form("nue_e_bin%d",i+1),i+1,i+1);
+    nue_templates_e_w[i]  = (TH1D*)nue_he_w->ProjectionY(Form("nue_w_e_bin%d",i+1),i+1,i+1);
+  }
+
+  TemplateFitter tf( CC_templates_m, CC_templates_m_nc, CC_templates_e, nue_templates_m, nue_templates_m_w, nue_templates_e, nue_templates_e_w, LEdep_m, LEdep_e );
+
+  double energy_bins[nbins_Ev+1];
+  for( int b = 0; b <= nbins_Ev; ++b ) {
+    energy_bins[b] = CC_he->GetXaxis()->GetBinLowEdge(b+1);
+  } 
+
+  TFile *f_fl = new TFile(Form("%s/flux_covmtr/flux_covmtr%d_120.root",data_path,nuCut),"READ");
+  TH2D *fl_cov = (TH2D*)f_fl->Get("hcv");
+  TFile *f_sig = new TFile(Form("%s/xS_covmtr/total_sigmtr%d_5sig_120.root",data_path,nuCut), "READ");
+  TH2D *sig_cov = (TH2D*)f_sig->Get("hcv");
+
+  double sig_bins[nbins+1][nbins+1], fl_bins[nbins+1][nbins+1];
+  for(int i=0; i<nbins; i++) {
+    for(int j=0; j<nbins; j++) {
+      fl_bins[i][j]    = fl_cov->GetBinContent(i+1, j+1);
+      sig_bins[i][j]   = sig_cov->GetBinContent(i+1, j+1);
+    }
+  }
+
+  tf.setEnergyBins( energy_bins );
+  tf.setCovmtr( fl_bins, sig_bins );
+
+  double seed[4], par_tgt[4], par_bf[4], par_no[4];
+
+  double Um42[8] = {1E-4, 1E-3, 1E-2, 0.02, 0.05, 0.1, 0.2, 0.5};
+  double Ut42[4] = {1E-4, 0.1, 0.3, 0.5};
+
+  par_tgt[0] = 0.04;
+  //par_tgt[1] = 0.01;
+  //par_tgt[2] = 0.5;
+  par_tgt[3] = 6.0;
+
+  for(int ii = 0; ii < 4; ii++) {
+    par_no[ii] = 0.;
+  }
+
+
+  for(int iUt=1; iUt<4; iUt++){
+    par_tgt[2] = Ut42[iUt];
+
+    for(int iUm=0; iUm<8; iUm++){
+      par_tgt[1] = Um42[iUm];
+      std::cout << par_tgt[0] << "\t" << par_tgt[1] << "\t" << par_tgt[2] << "\t" << par_tgt[3] << "\n";
+      tf.setPara( var, iUt, iUm, par_tgt, fitPara_m, fitPara_e );
+      tf.getTarget( par_tgt );
+      tf.Draw();
+    }
+  }
+
+
+}
+
+
---
+++ lownu_sterile/plot/chi2/TemplateFitter.h
@@ -0,0 +1,61 @@
+#ifndef TEMPLATEFITTER_H 
+#define TEMPLATEFITTER_H 
+
+#include "Math/Minimizer.h"
+#include "TH1.h"
+#include "TMatrixD.h"
+
+static const int nbins_Ev = 430;
+static const int nbins_CC = 56;
+static const int nbins_nue = 8;
+static const int nbins = 2*nbins_CC + nbins_nue;
+
+
+class TemplateFitter {
+
+  public:
+
+    TemplateFitter(TH1D * CC_templates_m[nbins_Ev], TH1D * CC_templates_m_nc[nbins_Ev], TH1D * CC_templates_e[nbins_Ev], TH1D * nue_templates_m[nbins_Ev], TH1D * nue_templates_m_w[nbins_Ev], TH1D * nue_templates_e[nbins_Ev], TH1D * nue_templates_e_w[nbins_Ev], TH1D * LEdep_m[29], TH1D * LEdep_e[29] );
+    ~TemplateFitter(){};
+    void setEnergyBins(double bins[nbins_Ev+1]);
+    void setCovmtr(double flmx_bct[nbins+1][nbins+1], double sigmx_bct[nbins+1][nbins+1]);
+    void setPara(char var[20], int iUt, int iUm, double par[4], double fitPara_m[29][7], double fitPara_e[29][7]);
+    void getTarget(double *par);
+    void Draw();
+    double getChi2(double *par);
+
+
+  private:
+    double getAvgPme(double energy, double Ue42, double Um42, double Ut42, double dm2, double ft[7]);
+    double getAvgPmt(double energy, double Ue42, double Um42, double Ut42, double dm2, double ft[7]);
+    double getAvgPee(double energy, double Ue42, double Um42, double Ut42, double dm2, double ft[7]);
+    double getAvgPmm(double energy, double Ue42, double Um42, double Ut42, double dm2, double ft[7]);
+
+    // The templates are reconstructed lepton energy, in a slice of true neutrino energy
+    TH1D * CC_m_templates[nbins_Ev];
+    // The templates are reconstructed lepton energy, in a slice of true neutrino energy no cuts
+    TH1D * CC_nc_m_templates[nbins_Ev];
+    // The template for the intrinsic nue
+    TH1D * CC_e_templates[nbins_Ev]; 
+    TH1D * nue_m_templates[nbins_Ev];
+    TH1D * nue_w_m_templates[nbins_Ev];
+    TH1D * nue_e_templates[nbins_Ev];
+    TH1D * nue_w_e_templates[nbins_Ev];
+    TH1D * LE_m[29];
+    TH1D * LE_e[29];
+    // define the energy bins used in the template
+    double m_energy_bins[nbins_Ev+1];
+    double s[4], fitP_m[29][7], fitP_e[29][7], ft_m[7], ft_e[7];
+    char *name;
+    int cutNu, it, im;
+    double b0,b1,b2,b3;
+    double s0,s1,s2,s3;
+
+    // this is the thing you are trying to fit to, i.e. the data distribution
+    TH1D * CCe_tgt = new TH1D();
+    TH1D * CCm_tgt = new TH1D();
+    TH1D * nue_tgt = new TH1D();
+
+};
+
+#endif
